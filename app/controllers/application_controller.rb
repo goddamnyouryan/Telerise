4378d561addc424240e1257142a627e98193fc1b
Ryan MacInnes <ryan.macinnes@gmail.com>
2011-06-06 00:38:37 -0700

    trying it now without that shit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :check_uri
   helper_method :current_user
-  
-  def check_uri
-    redirect_to request.protocol + "www." + request.host_with_port + request.request_uri if !/^www/.match(request.host) if Rails.env == 'production'
-  end
 
   private
 
