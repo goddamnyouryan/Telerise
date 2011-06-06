6f6df0c5e117dc3de2631629e8bd0436f263ddb5
Ryan MacInnes <ryan.macinnes@gmail.com>
2011-06-06 00:10:59 -0700

    redirect to www

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+  before_filter :check_uri
   helper_method :current_user
 
   private
+  
+  def check_uri
+    redirect_to request.protocol + "www." + request.host_with_port + request.request_uri if !/^www/.match(request.host) if Rails.env == 'production'
+  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
